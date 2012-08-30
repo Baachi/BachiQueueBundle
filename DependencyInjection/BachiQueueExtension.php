923fa84e617872f0ede1c11e59d5c109646c3c4f
Markus Bachmann <info@bachi.biz>
2012-08-30 22:17:48 +0200

    Add DependenyInjection support

---
+++ DependencyInjection/BachiQueueExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bachi\QueueBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+class BachiQueueExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
